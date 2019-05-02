778e85ab7782fce57055a6e3729ef131d95e1347
Justinus Andjarwirawan <justinus@gmail.com>
2019-05-02 15:00:54 +0700

    sw deployed

--- js/main.js
+++ js/main.js
@@ -38,4 +38,15 @@ function updateProduct(cat) {
 })
 }
 
-});
+})
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('sw.js').then(function(registration) {
+      console.log("ServiceWorker registration success with scope: ", registration.scope);
+    }, function(err) {
+      console.log("ServiceWorker registration failed: ", err)
+    })
+  })
+}
