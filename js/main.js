1553c53a7fe48e044ee796ead800ea232e4ab8cc
Justinus Andjarwirawan <justinus@gmail.com>
2019-05-02 14:21:22 +0700

    Template done

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+
+var _url = "https://my-json-server.typicode.com/gitjust/pwa/animals"
+
+var dataResults = ""
+var catResults = ""
+var categories = []
+
+$.get(_url, function(data) {
+  $.each(data, function(key, items) {
+    _cat = items.category
+    dataResults += "<div><h3>" + items.name + "</h3><p>" + _cat + "</p></div>"
+    if($.inArray(_cat, categories) == -1) {
+      categories.push(_cat)
+      catResults += "<option value='" + _cat +"'>" + _cat + "</option>"
+    }
+  })
+  $("#products").html(dataResults)
+  $("#cat_select").html("<option value='all'>semua</option>" + catResults)
+})
+
+$("#cat_select").on("change", function() {
+  updateProduct($(this).val())
+})
+
+function updateProduct(cat) {
+  console.log(cat)
+  var dataResults = ""
+  var _newUrl = _url
+  if (cat != "all")
+    _newUrl = _url + "?category=" + cat
+  $.get(_newUrl, function(data) {
+  $.each(data, function(key, items) {
+    _cat = items.category
+    dataResults += "<div><h3>" + items.name + "</h3><p>" + _cat + "</p></div>"
+  })
+  $("#products").html(dataResults)
+})
+}
+
+});
